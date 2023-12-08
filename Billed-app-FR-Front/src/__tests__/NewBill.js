42ac4dcd9da5eb045df93e89f90153b2f4888578
Mars375 <loic_2610@hotmail.fr>
2023-12-08 18:05:32 +0100

    feat: test 404 and 500

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -33,7 +33,6 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI();
       document.body.innerHTML = html;
 
-      // Assertions sur le rendu du formulaire
       expect(screen.getByTestId("form-new-bill")).toBeInTheDocument();
       expect(screen.getByTestId("file")).toBeInTheDocument();
       expect(screen.getByTestId("datepicker")).toBeInTheDocument();
@@ -143,53 +142,109 @@ describe("Given I am connected as an employee", () => {
       createSpy.mockRestore();
     })
   });
+});
 
-  describe("When I submit my form", () => {
-    test("Then it should create a bill for a valid form", () => {
+describe('Given I am connected as an employee', () => {
+  describe('When I complete the requested fields and I submit', () => {
+    afterEach(jest.clearAllMocks)
+    it('should call updateBill and navigate to Bills route', async () => {
+      // Arrange
+      const onNavigateMock = jest.fn();
       const newBill = new NewBill({
-        document,
-        onNavigate,
+        document: document,
+        onNavigate: onNavigateMock,
         store: mockStore,
-        localStorage: window.localStorage,
+        localStorage: localStorageMock,
       });
 
-      newBill.handleSubmit = jest.fn();
-      newBill.updateBill = jest.fn();
-      newBill.onNavigate = jest.fn();
+      const createSpy = jest.spyOn(mockStore.bills(), 'update');
 
-      const formValues = {
-        type: 'Services en ligne',
-        name: 'Abonnement Cloud',
-        amount: '240',
-        date: '2022-08-30',
-        vat: '40',
-        pct: '20',
-        commentary: 'Test commentary',
-      };
+      // Act
+      fireEvent.change(screen.getByTestId('expense-type'), { target: { value: 'Hôtel et logement' } });
+      fireEvent.change(screen.getByTestId('expense-name'), { target: { value: 'My Expense' } });
+      fireEvent.change(screen.getByTestId('amount'), { target: { value: '500' } });
+      fireEvent.change(screen.getByTestId('datepicker'), { target: { value: '2023-12-31' } });
+      fireEvent.change(screen.getByTestId('vat'), { target: { value: '80' } });
+      fireEvent.change(screen.getByTestId('pct'), { target: { value: '20' } });
+      fireEvent.change(screen.getByTestId('commentary'), { target: { value: 'This is a comment' } });
 
-      fireEvent.change(screen.getByTestId("expense-type"), { target: { value: formValues.type } });
-      fireEvent.change(screen.getByTestId("expense-name"), { target: { value: formValues.name } });
-      fireEvent.change(screen.getByTestId("datepicker"), { target: { value: formValues.date } });
-      fireEvent.change(screen.getByTestId("amount"), { target: { value: formValues.amount.toString() } });
-      fireEvent.change(screen.getByTestId("vat"), { target: { value: formValues.vat } });
-      fireEvent.change(screen.getByTestId("pct"), { target: { value: formValues.pct.toString() } });
-      fireEvent.change(screen.getByTestId("commentary"), { target: { value: formValues.commentary } });
-
-      fireEvent.submit(screen.getByTestId('form-new-bill'));
-
-      expect(newBill.updateBill).toHaveBeenCalledWith({
-        type: formValues.type,
-        name: formValues.name,
-        amount: parseInt(formValues.amount),
-        date: formValues.date,
-        vat: formValues.vat,
-        pct: parseInt(formValues.pct) || 20,
-        commentary: formValues.commentary,
-        fileUrl: null,
-        fileName: null,
-        status: 'pending',
+      await newBill.handleSubmit({
+        preventDefault: jest.fn(),
+        target: screen.getByTestId('form-new-bill'),
       });
-      expect(newBill.onNavigate).toHaveBeenCalledWith(ROUTES_PATH['Bills']);
+
+      // Assert
+      expect(createSpy).toHaveBeenCalledWith({
+        data: JSON.stringify({
+          type: 'Hôtel et logement',
+          name: 'My Expense',
+          amount: 500,
+          date: '2023-12-31',
+          vat: '80',
+          pct: 20,
+          commentary: 'This is a comment',
+          fileUrl: null,
+          fileName: null,
+          status: 'pending',
+        }),
+        selector: null,
+      });
+
+      await expect(await createSpy.mock.results[0].value).toEqual({
+        "id": "47qAXb6fIm2zOKkLzMro",
+        "vat": "80",
+        "fileUrl": "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        "status": "pending",
+        "type": "Hôtel et logement",
+        "commentary": "séminaire billed",
+        "name": "encore",
+        "fileName": "preview-facture-free-201801-pdf-1.jpg",
+        "date": "2004-04-04",
+        "amount": 400,
+        "commentAdmin": "ok",
+        "email": "a@a",
+        "pct": 20,
+      });
+
+      expect(onNavigateMock).toHaveBeenCalledWith(ROUTES_PATH['Bills']);
     });
-  })
+
+    it('should add a bill to API and fails with 404 message error', async () => {
+      const onNavigateMock = jest.fn();
+      const newBill = new NewBill({
+        document: document,
+        onNavigate: onNavigateMock,
+        store: mockStore,
+        localStorage: localStorageMock,
+      });
+
+      const updateSpy = jest.spyOn(mockStore.bills(), 'update').mockRejectedValueOnce({ status: 404, message: 'Not Found' });
+
+      await newBill.handleSubmit({
+        preventDefault: jest.fn(),
+        target: screen.getByTestId('form-new-bill'),
+      });
+
+      await expect(updateSpy.mock.results[0].value).rejects.toEqual({ status: 404, message: 'Not Found' });
+    });
+
+    it('should add a bill to API and fails with 500 message error', async () => {
+      const onNavigateMock = jest.fn();
+      const newBill = new NewBill({
+        document: document,
+        onNavigate: onNavigateMock,
+        store: mockStore,
+        localStorage: localStorageMock,
+      });
+
+      const updateSpy = jest.spyOn(mockStore.bills(), 'update').mockRejectedValueOnce({ status: 500, message: 'Internal Server Error' });
+
+      await newBill.handleSubmit({
+        preventDefault: jest.fn(),
+        target: screen.getByTestId('form-new-bill'),
+      });
+
+      await expect(updateSpy.mock.results[0].value).rejects.toEqual({ status: 500, message: 'Internal Server Error' });
+    });
+  });
 });
